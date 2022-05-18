78de3b4a0fcbcf1b789b39c6847623d52056a6fd
Noeyeddeer <john.e.mades@gmail.com>
2022-05-18 14:00:45 -0700

    Protected Index Complete

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of apartments" do
+      Apartment.create(
+        street: '3880 San Ramon Dr', 
+        city: 'Oceanside', 
+        state: 'California',
+        manager: 'Bob Smith',
+        email: 'bobsmith@hotmail.com',
+        price: '$3000',
+        bedrooms: 2, 
+        bathrooms: 1,
+        pets: 'No',
+        image: 'https://images1.forrent.com/i2/tb-tHmpjytjE7Kxrc1HHnqm26BNSFIVF5rwMKFAGatA/117/image.jpg',
+        user_id: 1
+      )
+
+      get '/apartments'
+
+      apartment = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      #expect(apartment.length).to eq 1
+    end
   end
 end
