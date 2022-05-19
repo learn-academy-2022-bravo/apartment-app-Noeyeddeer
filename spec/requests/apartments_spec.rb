afb4a543dfda76522f28b8592e254702faea0099
Noeyeddeer <john.e.mades@gmail.com>
2022-05-19 09:21:18 -0700

    apartments.spec.rb updated

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
   describe "GET /index" do
-    it "gets a list of apartments" do
-      Apartment.create(
+    it "lets a user see all apartments" do
+
+      user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+      user.apartments.create(
         street: '3880 San Ramon Dr', 
         city: 'Oceanside', 
         state: 'California',
@@ -13,15 +16,14 @@ RSpec.describe "Apartments", type: :request do
         bedrooms: 2, 
         bathrooms: 1,
         pets: 'No',
-        image: 'https://images1.forrent.com/i2/tb-tHmpjytjE7Kxrc1HHnqm26BNSFIVF5rwMKFAGatA/117/image.jpg',
-        user_id: 1
+        image: 'https://images1.forrent.com/i2/tb-tHmpjytjE7Kxrc1HHnqm26BNSFIVF5rwMKFAGatA/117/image.jpg'
       )
 
       get '/apartments'
 
       apartment = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      #expect(apartment.length).to eq 1
+      expect(apartment.length).to eq 1
     end
   end
 end
